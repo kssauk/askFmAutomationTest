eb2e3b4628110d853a35c6e7b8c13d321ec5a77f
kssauk <kseniya.sauk@gmail.com>
2015-12-28 13:47:58 +0300

    added timeout

--- src/test/java/steps/Steps.java
+++ src/test/java/steps/Steps.java
@@ -21,8 +21,8 @@ public class Steps {
 
     public void initBrowser() {
         driver = new FirefoxDriver();
-        driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(35, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(35, TimeUnit.SECONDS);
         logger.info("Browser started");
     }
 
